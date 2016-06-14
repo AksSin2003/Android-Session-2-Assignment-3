8c3edc735eb0ac7992e382600b6cac81ad97a108
AksSin2003 <akshitsinha@ymail.com>
2016-06-14 15:48:15 +0000

    Data Types
    Assignment 3 of session 2 in Android.

---
+++ integer.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Akshit
+ *
+ */
+public class integer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Two types are char and float.");
+
+	}
+
+}
